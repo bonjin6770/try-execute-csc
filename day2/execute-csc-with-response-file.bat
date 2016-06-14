05cd35d202cceec3a64b8bc815e03d8375c5596b
bonjin6770 <bonjin6770@gmail.com>
2016-06-15 00:12:33 +0900

    C#のコンパイラを触ってみよう 第二幕 "response file"

---
+++ day2/execute-csc-with-response-file.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem
+rem USAGE
+rem about this script
+rem
+
+rem variable settings
+set hoge = "•Ï”‚Ì’l"
+
+rem change current directory to this file's directory
+pushd %0\..
+cls
+
+rem main process
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe @try-response-file.rsp > result.log
+
+pause
+exit
